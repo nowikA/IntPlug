7779f78dde5e1bda5a2c63cdfea7261663334d0a
GitHub Action <action@github.com>
2021-12-28 12:19:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/nowika/intplug/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nowika/intplug/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nowika.intplug.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nowika.intplug.MyBundle
 
 class MyApplicationService {
 
